9531d5684c0aa6a84fe0cf59a8955a27a53d6f41
Lenka Černohorská <cernohorska.lc@gmail.com>
2023-03-29 21:10:16 +0200

    HOTOVO

--- index.js
+++ index.js
@@ -2,32 +2,39 @@
 const setAlarm = (event) => {
     event.preventDefault()
     let timeInput = Number(document.querySelector('.time-input').value)
-    let timeInputSec = (timeInput * 60)
-    let alarmMin = document.querySelector('.alarm__minutes')
-    let alarmSec = document.querySelector('.alarm__seconds')
-    let myAlarm = document.querySelector('.alarm')
-    let myAudio = document.querySelector('.audio')
-    alarmMin.textContent = String(timeInput).padStart(2, '0')
-    alarmSec.textContent = '00'
 
-    const deduct = () => {
-        if (timeInputSec > 0) {
-            timeInputSec = (timeInputSec - 1)
-            let minutes = Math.trunc(timeInputSec / 60)
-            let seconds = (timeInputSec % 60)
-            alarmMin.textContent = String(minutes).padStart(2, '0')
-            alarmSec.textContent = String(seconds).padStart(2, '0')
-        } else if (timeInputSec <= 0) {
-            clearInterval(timerId)
-            myAlarm.classList.add('alarm--ring')
-            myAudio.play()
+    if (Number.isInteger(timeInput) && (timeInput > 0) && (timeInput !== null)) {
+        let timeInputSec = (timeInput * 60)
+        let alarmMin = document.querySelector('.alarm__minutes')
+        let alarmSec = document.querySelector('.alarm__seconds')
+        let myAlarm = document.querySelector('.alarm')
+        let myAudio = document.querySelector('.audio')
+        alarmMin.textContent = String(timeInput).padStart(2, '0')
+        alarmSec.textContent = '00'
+
+        const deduct = () => {
+            if (timeInputSec > 0) {
+                timeInputSec = (timeInputSec - 1)
+                let minutes = Math.trunc(timeInputSec / 60)
+                let seconds = (timeInputSec % 60)
+                alarmMin.textContent = String(minutes).padStart(2, '0')
+                alarmSec.textContent = String(seconds).padStart(2, '0')
+            } else if (timeInputSec <= 0) {
+                clearInterval(timerId)
+                myAlarm.classList.add('alarm--ring')
+                myAudio.play()
+            }
         }
+        const timerId = setInterval(deduct, 1000)
+
+    } else {
+        alert('Zadej celé kladné číslo!')
     }
-    const timerId = setInterval(deduct, 1000)
 }
 
 let formular = document.querySelector('.controls')
 formular.addEventListener('submit', (event) => setAlarm(event))
+
 
 
 
